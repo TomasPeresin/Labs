2adc6a7d39f7b0f3b222c06dd09737f0433e78d0
Tomás Peresin <Tomas_Peresin@hotmail.com>
2022-03-19 15:42:44 -0300

    labs

---
+++ Unidad.1.Capitulo.1.Lab.1.Introduccion/Lab01/Lab01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola Mundo!");
+        }
+    }
+}
---
+++ Unidad.1.Capitulo.1.Lab.2.Plataforma/Lab02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor1 = "Este es el valor 1";
+            int valor2 = 5;
+            string valor3 = valor1;
+
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+            Console.WriteLine(valor3);
+            Console.WriteLine();
+            Console.WriteLine("Presionar tecla pa continuar");
+            Console.ReadKey();
+        }
+    }
+}
